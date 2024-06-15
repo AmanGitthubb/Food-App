d471e11bc04bd3a960225e88865220e57ecdb362
Aman Bhati <amanb3757@gmail.com>
2024-06-15 21:14:13 +0530

    Done

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Restaurants from "./Restaurants";
-import axios from "axios"
 
 const Body = () => {
 
@@ -11,13 +10,22 @@ const Body = () => {
       fetchData();
   } , [])
 
-const fetchData = () => {
-    axios.get("https://www.swiggy.com/mapi/homepage/getCards?lat=30.900965&lng=75.8572758").then((json) => {
-          const list = json?.data?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants;
-         setListOfRestaurants(list)
-         console.log(list);
+// const fetchData = () => {
+//     axios.get("https://www.swiggy.com/mapi/homepage/getCards?lat=23.2599333&lng=77.412615").then((json) => {
+//           const list = json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants;
+//          setListOfRestaurants(list)
+//          console.log(list);
 
-    })
+//     })
+// }
+
+const fetchData = async () => {
+    const data =  await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=23.2599333&lng=77.412615")
+    const json = await data.json()
+    const list = json?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants;
+    console.log(json);
+    setListOfRestaurants(list)
+    console.log(list);
 }
 
 
@@ -25,8 +33,9 @@ const fetchData = () => {
   
     return (
         <div className="flex justify-around flex-wrap">
-              {listOfRestaurants.map( (restaurants) => {
-         return  <Restaurants key={restaurants.info.id} info={restaurants.info}/>
+              { listOfRestaurants.map( (restaurants) => {
+                console.log(restaurants);
+         return <Restaurants key={restaurants.info.id} info={restaurants.info}/>
               })}
         </div>
     )
